1bc2162c6048d91f39f49d25089fcbd6cb508b3b
Speedfire667 <mateusportelag1@gmail.com>
2025-04-17 21:51:06 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 // vite.config.js
 module.exports = {
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html'
+    }
+  },
   preview: {
-    port: parseInt(process.env.PORT) || 4173
+    port: parseInt(process.env.PORT) || 4173,
+    host: '0.0.0.0',
+    open: true, // Vai abrir a URL automaticamente no navegador
   }
 };
